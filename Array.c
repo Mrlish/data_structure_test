92d3e57c027c127ba6aa9f1c2bafd621ade4d862
Timkong1 <876992202@qq.com>
2018-04-23 21:46:35 +0800

    Day10：数组

--- Array.c
+++ Array.c
@@ -46,6 +46,18 @@ Status InitArray(Array *A, int dim, ...)
 			return UNDERFLOW;
 		elemtotal *= A->bounds[i];
 	}
+	va_end(ap);
+	A->base = (ElemType*)malloc(elemtotal * sizeof(ElemType));
+	if (!A->base)
+		exit(OVERFLOW);
+	//求映像函数的常数ci并存入A.constants[i-1],i=1....dim
+	A->constants = (int*)malloc(dim * sizeof(int));
+	if (!A->constants)
+		exit(OVERFLOW);
+	A->constants[dim - 1] = 1;		//L=1,指针的增减以元素大小为单位
+	for (i = dim - 1;i >= 0;--i)
+		A->constants[i] = A->bounds[i + 1] * A->constants[i + 1];
+	return OK;
 }//InitArray
 
 
@@ -78,7 +90,7 @@ Status Locate(Array A, va_list ap, int *off)
 		ind = va_arg(ap, int);
 		if (ind < 0 || ind >= A.bounds[i])
 			return OVERFLOW;
-		*off += A.constants[i] * ind;
+		*off += A.constants[i]*ind;
 	}
 	return OK;
 }
@@ -91,8 +103,8 @@ Status Value(Array A, ElemType *e, ...)
 	int off;
 	Status result;
 	va_list ap;
-	va_start(ap, e);
-	if ((result = Locate(A, ap, &off)) < -0)
+	va_start(ap, *e);
+	if ((result = Locate(A, ap, &off)) <=0)
 		return result;
 	*e = *(A.base + off);
 	return OK;
@@ -101,5 +113,11 @@ Status Value(Array A, ElemType *e, ...)
 
 int main(void)
 {
-
+	Array a1;
+	int off;
+	ElemType a;
+	InitArray(&a1, 3, 3, 3, 3);
+	Value(a1, &a, 1, 1, 1);
+	printf("%d", a);
+	system("pause");
 }
